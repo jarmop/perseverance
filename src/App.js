94af2cda7f03662e2d47d5187ecc3fe4808d8841
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2021-04-04 22:43:07 +0300

    Tweak styles

--- src/App.js
+++ src/App.js
@@ -7,29 +7,29 @@ const IMAGE_HEIGHT = 2127
 const IMAGE_ASPECT_RATIO = IMAGE_WIDTH / IMAGE_HEIGHT
 const IMAGE_ELEMENT_HEIGHT = 500
 const IMAGE_ELEMENT_WIDTH = IMAGE_ASPECT_RATIO * IMAGE_ELEMENT_HEIGHT
-const CANVAS_SIZE = 200
+const CANVAS_SIZE = 500
 const CANVAS_CENTER = {
   X: CANVAS_SIZE / 2,
   Y: CANVAS_SIZE / 2,
 }
-const CIRCLE_RADIUS = CANVAS_SIZE / 2
+// Make circle just large enough to go outside of canvas
+const CIRCLE_RADIUS = Math.sqrt(2 * Math.pow(CANVAS_SIZE / 2, 2))
 
 const draw = (ctx, background, viewAngleRight, viewAngleLeft) => {
   ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
   ctx.beginPath()
   ctx.drawImage(background, 0, 0)
-  ctx.arc(CANVAS_CENTER.X, CANVAS_CENTER.Y, CIRCLE_RADIUS, 0, 2 * Math.PI)
   ctx.moveTo(CANVAS_CENTER.X, CANVAS_CENTER.Y)
-  ctx.lineTo(
-    CANVAS_CENTER.X + CIRCLE_RADIUS * Math.cos(viewAngleLeft),
-    CANVAS_CENTER.Y + CIRCLE_RADIUS * Math.sin(viewAngleLeft)
+  ctx.arc(
+    CANVAS_CENTER.X,
+    CANVAS_CENTER.Y,
+    CIRCLE_RADIUS,
+    viewAngleRight,
+    viewAngleLeft
   )
-  ctx.moveTo(CANVAS_CENTER.X, CANVAS_CENTER.Y)
-  ctx.lineTo(
-    CANVAS_CENTER.X + CIRCLE_RADIUS * Math.cos(viewAngleRight),
-    CANVAS_CENTER.Y + CIRCLE_RADIUS * Math.sin(viewAngleRight)
-  )
-  ctx.stroke()
+  ctx.closePath()
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.15)'
+  ctx.fill()
 }
 
 const getPositionInRadians = (position) => {
@@ -44,7 +44,7 @@ function App() {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
     const background = new Image()
-    background.src = 'jezero.png'
+    background.src = 'jezero500.png'
     background.onload = () => {
       draw(ctx, background, getLeftInRadians(), getRightInRadians())
     }
@@ -55,7 +55,7 @@ function App() {
 
   return (
     <div className="App">
-      <img src={image} alt=""></img>
+      <img src={image} alt="" height={IMAGE_ELEMENT_HEIGHT}></img>
       <canvas id="canvas" width={CANVAS_SIZE} height={CANVAS_SIZE}></canvas>
     </div>
   )
